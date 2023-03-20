f61c8e29ddbc92ddcd9cebf9786edb780a8497a4
Martin Antonio Unland Elorrieta <m.unland@uni-muenster.de>
2023-03-20 17:15:11 +0100

    check alloewd

--- scanner_motor_control/scanner_motor_manager.py
+++ scanner_motor_control/scanner_motor_manager.py
@@ -96,7 +96,7 @@ class ScannerControl:
                 zip(self.motors.values(), distances),
             )
 
-    def check_position_in_mm_valid(self, positions: list, relative: bool = False) -> List[bool]:
+    def check_position_in_mm_allowed(self, positions: list, relative: bool = False) -> List[bool]:
         valid = []
         for motor, position in zip(self.motors.values(), positions):
             bl, _ = motor.check_if_position_in_mm_allowed(position)
