e2c19776e34da9eabd2702e52a7f333e57c5c85b
Martin Antonio Unland Elorrieta <m.unland@uni-muenster.de>
2023-03-20 08:48:53 +0100

    v1

--- scanner_motor_control.py
+++ scanner_motor_control.py
@@ -10,6 +10,13 @@ class ScannerControl:
         self.motors = {}
 
     def connect(self, ports: list, baudrate=9600) -> None:
+        """
+        Connect to each motor on the provided ports with the specified baudrate.
+
+        Args:
+            ports (list): A list of ports to connect to the motors.
+            baudrate (int, optional): Baudrate for the serial connection. Defaults to 9600.
+        """
         for i, port in enumerate(ports):
             self.motors[i] = Motor(port, i, baudrate)
         
@@ -21,6 +28,13 @@ class ScannerControl:
             pool.map(lambda motor: motor.disconnect(), self.motors.values())
 
     def set_motors_speed(self, max_speed: int = 5000, max_acceleration: int = 1500):
+        """
+        Set the maximum speed and acceleration for all motors.
+
+        Args:
+            max_speed (int, optional): Maximum speed for the motors. Defaults to 5000.
+            max_acceleration (int, optional): Maximum acceleration for the motors. Defaults to 1500.
+        """
         for motor in self.motors.values():
             motor.set_speed_and_acceleration(max_speed=max_speed, max_acceleration=max_acceleration)
 
@@ -36,14 +50,24 @@ class ScannerControl:
         with ThreadPool(processes=len(self.motors)) as pool:
             pool.map(lambda motor: motor.deactivate_stall_guard(), self.motors.values())
 
-    def move_motors_to_position(self, positions: list):
+    def move_motors_to_position(self, positions: list) -> None:
+        """
+        Move each motor to its respective position in the given list of positions in parallel.
+
+        Args:
+            positions (list): A list of positions for each motor, [pos_motor_0, pos_motor_1, pos_motor_2].
+        """
         with ThreadPool(processes=len(self.motors)) as pool:
-            pool.starmap(lambda motor, position: motor.move_to_position(position), zip(self.motors.values(), positions))
+            pool.starmap(lambda motor, position: motor.move_to_step(position), zip(self.motors.values(), positions))
 
-    def configure_motors(self, config: dict):
-        for motor in self.motors.values():
-            motor.configure(config)
 
+    # I don't know if we will ever use it, but the following two methods allow use to write:
+    # "with ScannerControl() as scanner:
+    #   scanner.connect(ports, baudrate)
+    #   scanner.set_motors_speed()
+    #   ..."
+    # And when we get out the with block, the scanner will automatically disconnect.
+    # It is good to manage resources more effectively, but I don't know if we really need it in this case...
     def __enter__(self):
         return self
 
